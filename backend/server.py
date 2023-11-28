98c46a5de6fd832591cb12c5e333721135e6b325
choimoonwon <yellowbill@snu.ac.kr>
2023-11-28 18:16:00 +0900

    front

--- backend/server.py
+++ backend/server.py
@@ -5,10 +5,9 @@ import json
 import openai
 from flask import Flask, render_template, request, redirect, session, jsonify
 import os.path
-from ML.ml_main import ML_result
 
 app = Flask(__name__)
-openai.api_key = "api here"
+openai.api_key = "api-here"
 
 FRONT_ENDSYMBOL = '0'
 
